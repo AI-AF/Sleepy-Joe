4742a28597b873a2d40a081395e3b39b3b962b79
Ruben Flam-Shepherd (MacBook Air) <ruben.flam-shepherd@gmail.com>
2020-04-05 14:53:48 -0400

    black-ened python files

--- src/config.py
+++ src/config.py
@@ -2,10 +2,10 @@
 
 import os
 
-JOE_ID = '22BG2D'
+JOE_ID = "22BG2D"
 
-JOE_SHH = '4f89a9d66af197a0a910260ea041224d'
+JOE_SHH = "4f89a9d66af197a0a910260ea041224d"
 
 _CONFIG_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-TOKEN_PATH = os.path.join(_CONFIG_DIR_PATH, 'tokens')
+TOKEN_PATH = os.path.join(_CONFIG_DIR_PATH, "tokens")
--- src/gringotts.py
+++ src/gringotts.py
@@ -21,8 +21,7 @@ class OAuth2Server:
     """ OAuth Server class
     """
 
-    def __init__(self, client_id, client_secret,
-                 redirect_uri='http://127.0.0.1:8080/'):
+    def __init__(self, client_id, client_secret, redirect_uri="http://127.0.0.1:8080/"):
         """ Initialize the FitbitOauth2Client """
         self.success_html = """
             <h1>You are now authorized to access the Fitbit API!</h1>
@@ -31,10 +30,7 @@ class OAuth2Server:
             <h1>ERROR: %s</h1><br/><h3>You can close this window</h3>%s"""
 
         self.fitbit = Fitbit(
-            client_id,
-            client_secret,
-            redirect_uri=redirect_uri,
-            timeout=10,
+            client_id, client_secret, redirect_uri=redirect_uri, timeout=10,
         )
 
         self.redirect_uri = redirect_uri
@@ -50,8 +46,12 @@ class OAuth2Server:
 
         # Same with redirect_uri hostname and port.
         urlparams = urlparse(self.redirect_uri)
-        cherrypy.config.update({'server.socket_host': urlparams.hostname,
-                                'server.socket_port': urlparams.port})
+        cherrypy.config.update(
+            {
+                "server.socket_host": urlparams.hostname,
+                "server.socket_port": urlparams.port,
+            }
+        )
 
         cherrypy.quickstart(self)
 
@@ -67,19 +67,20 @@ class OAuth2Server:
                 self.fitbit.client.fetch_access_token(code)
             except MissingTokenError:
                 error = self._fmt_failure(
-                    'Missing access token parameter.</br>Please check that '
-                    'you are using the correct client_secret')
+                    "Missing access token parameter.</br>Please check that "
+                    "you are using the correct client_secret"
+                )
             except MismatchingStateError:
-                error = self._fmt_failure('CSRF Warning! Mismatching state')
+                error = self._fmt_failure("CSRF Warning! Mismatching state")
         else:
-            error = self._fmt_failure('Unknown error while authenticating')
+            error = self._fmt_failure("Unknown error while authenticating")
         # Use a thread to shutdown cherrypy so we can return HTML first
         self._shutdown_cherrypy()
         return error if error else self.success_html
 
     def _fmt_failure(self, message):
         tb = traceback.format_tb(sys.exc_info()[2])
-        tb_html = '<pre>%s</pre>' % ('\n'.join(tb)) if tb else ''
+        tb_html = "<pre>%s</pre>" % ("\n".join(tb)) if tb else ""
         return self.failure_html % (message, tb_html)
 
     def _shutdown_cherrypy(self):
@@ -111,16 +112,16 @@ def save_token(token):
             }`
     """
 
-    print('SAVING TOKEN\n=====\n')
+    print("SAVING TOKEN\n=====\n")
 
-    file_name = "{}_token.pkl".format(token['user_id'])
+    file_name = "{}_token.pkl".format(token["user_id"])
 
     full_file_path = os.path.join(config.TOKEN_PATH, file_name)
 
     with open(full_file_path, "wb") as curr_file:
         pickle.dump(token, curr_file)
 
-    print('TOKEN SAVED TO {} \n=====\n'.format(full_file_path))
+    print("TOKEN SAVED TO {} \n=====\n".format(full_file_path))
 
 
 def load_token(user_id):
@@ -137,17 +138,19 @@ def load_token(user_id):
 
     full_file_path = os.path.join(config.TOKEN_PATH, file_name)
 
-    with open(full_file_path, 'rb') as handle:
+    with open(full_file_path, "rb") as handle:
         token_dict = pickle.load(handle)
 
     return token_dict
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import config
 
     if len(sys.argv) == 3:
-        CLIENT_ID, CLIENT_SECRET = sys.argv[1:]  # pylint: disable=unbalanced-tuple-unpacking
+        CLIENT_ID, CLIENT_SECRET = sys.argv[
+            1:
+        ]  # pylint: disable=unbalanced-tuple-unpacking
     elif len(sys.argv) == 1:
         CLIENT_ID, CLIENT_SECRET = config.JOE_ID, config.JOE_SHH
     else:
@@ -159,13 +162,16 @@ if __name__ == '__main__':
     SERVER.browser_authorize()
 
     PROFILE = SERVER.fitbit.user_profile_get()
-    print('You are authorized to access data for the user: {}'.format(
-        PROFILE['user']['fullName']))
+    print(
+        "You are authorized to access data for the user: {}".format(
+            PROFILE["user"]["fullName"]
+        )
+    )
 
-    print('TOKEN\n=====\n')
+    print("TOKEN\n=====\n")
 
     TOKEN = SERVER.fitbit.client.session.token
     for key, value in TOKEN.items():
-        print('{} = {}'.format(key, value))
+        print("{} = {}".format(key, value))
 
     save_token(TOKEN)
